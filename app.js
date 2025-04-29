bd49fef6a55893db73e9c01528167d3fbb11d5d8
Sheryar <alisharyar93@gmail.com>
2025-04-29 21:42:19 +0500

    Server

--- app.js
+++ app.js
@@ -30,7 +30,6 @@ mongoose
     app.use(express.static(path.join(__dirname, "public")));
     app.use(express.static('public'));
 
-    // app.set('view engine', 'ejs');
     // app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.render('index');
